15ccee4011da793320d52a11a04509a2e7f0d80a
BuildTools <lenny.angst@easyid.ch>
2020-12-01 16:26:21 +0100

    Setup ClientSession Update gitignore

--- server/src/main/java/com/lezurex/whatsweb/server/connectivity/ClientSession.java
+++ server/src/main/java/com/lezurex/whatsweb/server/connectivity/ClientSession.java
@@ -1,8 +1,27 @@
 package com.lezurex.whatsweb.server.connectivity;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class ClientSession {
-    public ClientSession(Socket socket) {
+
+    private Socket socket;
+    private OutputStream out;
+    private InputStreamReader in;
+
+    public ClientSession(Socket socket) throws IOException {
+        this.socket = socket;
+        out = socket.getOutputStream();
+        in = new InputStreamReader(socket.getInputStream());
+        startListener();
     }
+
+    public void startListener() {
+        while (socket.isConnected()) {
+
+        }
+    }
+
 }
