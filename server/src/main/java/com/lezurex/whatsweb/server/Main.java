13c726f403135e9cbf9c75cca532b5ac14b1af6f
BuildTools <lenny.angst@easyid.ch>
2020-12-15 15:30:15 +0100

    Change websocket library

--- server/src/main/java/com/lezurex/whatsweb/server/Main.java
+++ server/src/main/java/com/lezurex/whatsweb/server/Main.java
@@ -1,12 +1,11 @@
 package com.lezurex.whatsweb.server;
 
-import com.lezurex.whatsweb.server.connectivity.WebSocket;
-
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new WebSocket(2121);
+        Server server = new Server(2121);
+        server.start();
     }
 }
